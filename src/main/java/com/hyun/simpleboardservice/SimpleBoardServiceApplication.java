12292018b339a1dd558dd1bfaf73dccc09e16ccf
hyun <itsmine096@gmail.com>
2023-11-09 22:09:12 +0900

    Project init

---
+++ src/main/java/com/hyun/simpleboardservice/SimpleBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.simpleboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyun/simpleboardservice/SimpleBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.simpleboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
